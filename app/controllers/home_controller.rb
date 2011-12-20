6be0ab476c63af9f519544b64ccbd853e8be4c95
Jim Geurts <jim@biacreations.com>
2011-12-19 20:02:23 -0600

    hack in some error help

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,12 +4,16 @@ class HomeController < ApplicationController
       if cookies[:has_submitted].present?
         return redirect_to thank_you_path
       end
-      p = params['signed_request'].split('.')[1]
-      json = Base64.decode64(p + "=" * (4 - p.size % 4))
-      data = ActiveSupport::JSON.decode(json)
-      liked = data["page"]["liked"]
-      if liked
-        return redirect_to new_submission_path
+      begin
+        p = params['signed_request'].split('.')[1]
+        json = Base64.decode64(p + "=" * (4 - p.size % 4))
+        data = ActiveSupport::JSON.decode(json)
+        liked = data["page"]["liked"]
+        if liked
+          return redirect_to new_submission_path
+        end
+      rescue
+        @error = "#{$!}"
       end
     end
   end
